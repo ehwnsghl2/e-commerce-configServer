93bc3c60dbbe96a54982303dddbad68b4e64276b
도준회 <junhoe@fastviewkorea.com>
2022-04-12 19:43:35 +0900

    config 설정 추가

--- src/main/kotlin/com/brandjunhoe/config/ConfigApplication.kt
+++ src/main/kotlin/com/brandjunhoe/config/ConfigApplication.kt
@@ -2,10 +2,10 @@ package com.brandjunhoe.config
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.config.server.EnableConfigServer
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableConfigServer
 @SpringBootApplication
 class ConfigApplication
